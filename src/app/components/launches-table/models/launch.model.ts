037a72da12985d19852117f8fbb2addce2f607a9
Marcin Żuk <Gnoneks@gmail.com>
2024-11-08 15:28:54 +0100

    002--Create launch models

---
+++ src/app/components/launches-table/models/launch.model.ts
@@ -0,0 +1,11 @@
+
+
+export interface Launch {
+    id: number;
+    country_code: string;
+    map_image: string;
+    name: string;
+    total_landing_count: number;
+    total_launch_count: number;
+    url: string;
+}
