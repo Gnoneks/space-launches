89f23db66b82b24cbdd7d0d0979f45efb1885e24
Marcin Żuk <Gnoneks@gmail.com>
2024-11-08 15:29:09 +0100

    002--Set up first http request

--- src/app/components/launches-table/launches-table.component.ts
+++ src/app/components/launches-table/launches-table.component.ts
@@ -1,12 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { LaunchesTableService } from './launches-table.service';
+
 
 @Component({
   selector: 'app-launches-table',
   standalone: true,
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './launches-table.component.html',
-  styleUrl: './launches-table.component.scss'
+  styleUrl: './launches-table.component.scss',
+  providers: [LaunchesTableService]
 })
-export class LaunchesTableComponent {
+export class LaunchesTableComponent implements OnInit{
+
+  constructor(private readonly _launchesTableService: LaunchesTableService) {}
+
+  ngOnInit() {
+    this._launchesTableService.getLaunchList().subscribe((v) => console.log(v))
+  }
 
 }
--- src/app/components/launches-table/launches-table.service.ts
+++ src/app/components/launches-table/launches-table.service.ts
@@ -1,1 +1,13 @@
-//TODO Fetching data
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LaunchList } from './models/launch-list.model';
+
+@Injectable()
+export class LaunchesTableService {
+  constructor(private http: HttpClient) {}
+
+  getLaunchList(): Observable<LaunchList> {
+    return this.http.get<LaunchList>('https://ll.thespacedevs.com/2.0.0/location/');
+  }
+}
